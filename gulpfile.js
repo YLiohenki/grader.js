dfdee7c0fb74a6e125feea79138e1903d95f178c
Yauheni Liohenki <legenkiyes@gmail.com>
2016-09-15 18:00:07 +0300

    added scss support
    added scss support

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,81 @@
 'use strict';
 
 var gulp = require('gulp'),
-	browserify = require('browserify'),
-	uglify = require('gulp-uglify'),
-	source = require('vinyl-source-stream'),
-	buffer = require('vinyl-buffer'),
-	rename = require('gulp-rename'),
-	gulpif = require('gulp-if'),
-	pug = require('gulp-pug'),
-	copy = require('gulp-copy'),
-	sourcemaps = require('gulp-sourcemaps'),
-	header = require('gulp-header'),
-	isSourcemaps = require('minimist')(process.argv.slice(2)).sourcemaps,
-	appVersion = require('./package.json').version;
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    rename = require('gulp-rename'),
+    gulpif = require('gulp-if'),
+		sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    copy = require('gulp-copy'),
+    sourcemaps = require('gulp-sourcemaps'),
+    header = require('gulp-header'),
+    isSourcemaps = require('minimist')(process.argv.slice(2)).sourcemaps,
+    appVersion = require('./package.json').version;
 
 
 // LIB
 gulp.task('build', function() {
-	return browserify({ entries: 'standalone.js', debug: isSourcemaps }).bundle()
-		.pipe(source('./grader.js'))
-		.pipe(buffer())
-		.pipe(gulpif(!isSourcemaps, header('/*! Grader v' + appVersion + ' */\n')))
-		.pipe(gulp.dest('./dist/'));
+    return browserify({
+            entries: 'standalone.js',
+            debug: isSourcemaps
+        }).bundle()
+        .pipe(source('./grader.js'))
+        .pipe(buffer())
+        .pipe(gulpif(!isSourcemaps, header('/*! Grader v' + appVersion + ' */\n')))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('buildMin', ['build'], function() {
-	return gulp.src('./dist/grader.js')
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(uglify({ preserveComments: 'license' }))
-		.pipe(gulp.dest('./dist/'));
+    return gulp.src('./dist/grader.js')
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify({
+            preserveComments: 'license'
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('docDist', ['buildMin'], function() {
-	return gulp.src('./dist/grader.js')
-		.pipe(copy('./docs/assets/js/', { prefix: 1 }));
+    return gulp.src('./dist/grader.js')
+        .pipe(copy('./docs/assets/js/', {
+            prefix: 1
+        }));
 });
 
 gulp.task('buildDoc:html', function() {
-	return gulp.src('./docs/assets/pug/*.pug')
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest('./docs/'));
+    return gulp.src('./docs/assets/pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./docs/'));
+});
+
+gulp.task('buildDoc:css', function() {
+	return gulp.src('./docs/assets/css/scss/index.scss')
+		.pipe(gulpif(isSourcemaps, sourcemaps.init({ loadMaps: true })))
+		.pipe(sass({ outputStyle: 'compressed' })
+			.on('error', sass.logError)
+		)
+		.pipe(gulpif(isSourcemaps, sourcemaps.write({ includeContent: true })))
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('./docs/assets/css/'))
 });
 
 // TASKS
-	// lib
-  gulp.task('default', ['build', 'buildMin', 'docDist']);
+// lib
+gulp.task('default', ['build', 'buildMin', 'docDist']);
 
-  gulp.task('watch', ['default'], function() {
-  	gulp.watch('./lib/**/*.js', ['default'])
-  });
+gulp.task('watch', ['default'], function() {
+    gulp.watch('./lib/**/*.js', ['default'])
+});
+
+gulp.task('buildDoc', ['buildDoc:html', 'buildDoc:css']);
+
+gulp.task('watchDoc', ['buildDoc'], function() {
+	gulp.watch('./docs/assets/pug/**/*', ['buildDoc:html']);
+	gulp.watch('./docs/assets/css/scss/**/*', ['buildDoc:css']);
+});
